5c6d0aca2bd248813b24f6e1422e897af1db6ee8
Julio Fonseca <juliocsrmf@gmail.com>
2022-08-21 17:20:03 -0300

    base project

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import * as C from './App.styles';
+import logoImage from './assets/devmemory_logo.png';
+
+const App = () => {
+    return (
+        <C.Container>
+            <C.Info>
+                <C.LogoLink href=''>
+                    <img src={logoImage} width='200' alt='' />
+                </C.LogoLink>
+
+                <C.InfoArea>...</C.InfoArea>
+
+                <button>Reiniciar</button>
+            </C.Info>
+
+            <C.GridArea></C.GridArea>
+        </C.Container>
+    );
+};
+
+export default App;
